bff77b71da1a09e24951d079cb20abe6f6eec37f
CalebFilip <filip0732caleb@gmail.com>
2024-09-23 15:43:45 -0400

    feat: added all operator functions

---
+++ goodCalculator.py
@@ -0,0 +1,15 @@
+#takes two numbers and returns sum
+def add(addend1, addend2):
+    return addend1 + addend2
+
+#takes two numbers and returns difference
+def subtract(minuend, subtrahend):
+    return minuend - subtrahend
+
+#takes two numbers and returns product
+def multiply(factor1, factor2):
+    return factor1 * factor2
+
+#takes two numbers and returns quotient
+def divide(dividend, divisor):
+    return dividend / divisor
