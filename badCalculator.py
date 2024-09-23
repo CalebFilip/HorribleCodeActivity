4e43260e0d73804e6b91c3d09e7577f9e0adaa44
CalebFilip <filip0732caleb@gmail.com>
2024-09-23 15:15:14 -0400

    first commit

---
+++ badCalculator.py
@@ -0,0 +1,27 @@
+def add(a, c):
+    d = 0
+    while c < d:
+        a += 1
+        d +=1
+
+#getting input for first number
+a = input('Enter First Number: ')
+#getting operator
+b = input('Enter Operator(+, -, /, *): ')
+#getting input for second number
+c=input('Enter Second Number: ')
+
+#checking to see what operator was inputted
+if b == '+':
+    print(add(a,c))
+
+#checking to see what operator was inputted
+elif b == '-':
+
+
+#checking to see what operator was inputted
+elif b == '/':
+
+
+#checking to see what operator was inputted
+elif b == '*':
